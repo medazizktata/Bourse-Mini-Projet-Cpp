f63a1b14e3c65d90c791b3de527cf57e5f48ee7d
medazizktata <aziiizktata7@gmail.com>
2023-04-16 00:54:31 +0100

    Surcharge operateur ==

--- Main file.cpp
+++ Main file.cpp
@@ -7,20 +7,23 @@ class Date {
         int mois;
         int annee;
     public:
-        Date();
         Date(int j, int m, int a);
         int get_jour();
         int get_mois();
         int get_annee();
+        bool cntrl_jour(int j) const;
+        bool cntrl_mois(int m)const;
+        bool cntrl_annee(int a) const;
+        bool cntrl_format() const;
         Date incrementer(Date &d);
         Date decrementer(Date &d);
         friend ostream& operator<<(ostream& os, const Date& d);
         friend istream& operator>>(istream& os, Date& d);
-        Date operator <(const Date& d);
-        Date operator +(const Date& d);
-        Date operator ==(const Date& d);
+        bool operator<(const Date& d);
+        Date operator+(const Date& d);
+        bool operator==(const Date& d);
 };
-Date::Date(int j, int m, int a){
+Date::Date(int j=0, int m=0, int a=0){
     jour=j;
     mois= m;
     annee=a;
@@ -39,16 +42,47 @@ ostream& operator<<(ostream& os, const Date& d){
     return os;
 }
 istream& operator>>(istream& is, Date& d){
-    string input;
-    getline(is, input);
     char slash;
     is >> d.jour >> slash >> d.mois >> slash >> d.annee;
     return is;
 }
+bool Date::operator==(const Date& d){
+    return ((jour== d.jour) && (mois==d.mois) && (annee==d.annee));
+}
+bool Date::cntrl_jour(int j) const{
+   return (j<=31 && j>0);
+}
+bool Date::cntrl_mois(int m) const{
+    return (m<=12 && m>0);
+}
+bool Date::cntrl_annee(int a) const{
+    return (a<=2023 && a>2000);
+}
+bool Date::cntrl_format() const{
+    return ((cntrl_jour(jour)) && (cntrl_mois(mois)) && (cntrl_annee(annee)));
+}
 int main(){
     Date d;
-    cout<<"Entrer une date: (jj/mm/aaaa)";
-    cin>>d;
+    bool date_valide=false;
+    int c=0;
+    cout<<"Entrer une date sous le format suivant (jj/mm/aaaa) : ";
+    while (!date_valide){
+        cin>>d;
+        if (d.cntrl_format()){
+            date_valide=true;
+        }
+        else {
+            c++;
+            cout<<"Format incorrect"<<endl;
+            cout<<"Veuillez re-entrer une date valide sous le format suivant (jj/mm/aaaa) : ";
+        }
+    }
+    if (c!=0){
+        cout<<"Format Valide"<<endl;
+    }
+    cout<<"Le jour est:"<<d.get_jour()<<endl;
+    cout<<"Le mois est:"<<d.get_mois()<<endl;
+    cout<<"L'annee est:"<<d.get_annee()<<endl;
     cout<<"La date est:"<<d;
     return 0;
 }
