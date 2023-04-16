1cea2acb340b9cdd6437bfc17c619e8c32d160d0
medazizktata <aziiizktata7@gmail.com>
2023-04-16 01:50:47 +0100

    methode incrementer

--- Main file.cpp
+++ Main file.cpp
@@ -15,6 +15,9 @@ class Date {
         bool cntrl_mois(int m)const;
         bool cntrl_annee(int a) const;
         bool cntrl_format() const;
+        void set_jour(int nv_jour);
+        void set_mois(int nv_mois);
+        void set_annee(int nv_annee);
         Date incrementer(Date &d);
         Date decrementer(Date &d);
         friend ostream& operator<<(ostream& os, const Date& d);
@@ -71,6 +74,110 @@ bool Date::cntrl_annee(int a) const{
 }
 bool Date::cntrl_format() const{
     return ((cntrl_jour(jour)) && (cntrl_mois(mois)) && (cntrl_annee(annee)));
+}
+void Date::set_jour(int nv_jour){
+    if(cntrl_jour(nv_jour)){
+        jour=nv_jour;
+    }
+}
+void Date::set_mois(int nv_mois){
+    if(cntrl_mois(nv_mois)){
+        mois=nv_mois;
+    }
+}
+void Date::set_annee(int nv_annee){
+    if(cntrl_annee(nv_annee)){
+        annee=nv_annee;
+    }
+}
+bool annee_bissextile(int annee) {
+    if (annee % 4 == 0) {
+        if (annee % 100 == 0) {
+            if (annee % 400 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+Date incrementer(Date &d){
+    int jour=d.get_jour();
+    int mois=d.get_mois();
+    int annee=d.get_annee();
+    int fev=28;
+    if (mois<8){
+        if (mois%2==0){
+            if (mois!=2){
+                if(jour<30){
+                    d.set_jour(jour++);
+                }
+                else {
+                    d.set_jour(1);
+                    d.set_mois(mois++);
+                }
+            }
+            if (mois==2){
+                if (annee_bissextile(annee)){
+                    fev=29;
+                }
+                if(jour<fev){
+                    d.set_jour(jour++);
+                }
+                else {
+                    d.set_jour(1);
+                    d.set_mois(mois++);
+                }
+            }
+        }
+        else {
+            if(jour<31){
+                    d.set_jour(jour++);
+                }
+                else {
+                    d.set_jour(1);
+                    d.set_mois(mois++);
+                }
+        }
+    }
+    else if (mois!=12) {
+        if (mois%2==0){
+            if(jour<31){
+                d.set_jour(jour++);
+            }
+            else {
+                d.set_jour(1);
+                d.set_mois(mois++);
+            }
+        }
+        else {
+            if(jour<30){
+                d.set_jour(jour++);
+            }
+            else {
+                d.set_jour(1);
+                d.set_mois(mois++);
+            }
+        }
+    }
+    else {
+        if(jour<31){
+            d.set_jour(jour++);
+        }
+        else {
+            d.set_jour(1);
+            d.set_mois(mois++);
+            d.set_annee(annee++);
+        }
+    }
 }
 int main(){
     Date d;
