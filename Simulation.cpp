2cd6d36e84176c953c887a8b5a6915bd1c299c82
medazizktata <aziiizktata7@gmail.com>
2023-05-15 13:54:07 +0100

    Update Simulation

--- Simulation.cpp
+++ Simulation.cpp
@@ -2,77 +2,101 @@
 #include <string>
 #include <chrono>
 #include <map>
-#include "Date.h"
+#include <set>
 #include "Portefeuille.h"
 #include "Trader.h"
 #include "Bourse.h"
 #include "BourseVector.h"
+#include "BourseSet.h"
+#include "BourseMultimap.h"
 using namespace std;
 using namespace chrono;
+template <typename typebourse>
 class Simulation
 {
-private:
-    static Portefeuille portefeuilleTrader;
 
 public:
-    Simulation(Portefeuille p)
-    {
-        portefeuilleTrader = p;
-    }
-    Portefeuille get_portfeuille()
-    {
-        return portefeuilleTrader;
-    }
-    static map<string, long> lancer_simulation(Bourse &B, Trader &T, Date date_deb, Date date_fin, double solde=0)
+    Simulation() {}
+    static map<string, long> lancer_simulation(typebourse &B, Trader<typebourse> &T, Date date_deb, Date date_fin, double solde = 0)
     {
         map<string, long> stats, prix_vente;
         Titre nth;
         Transaction tr(RIEN, nth);
         Date dateact(date_deb.get_jour(), date_deb.get_mois(), date_deb.get_annee());
         B.set_auj(dateact);
-        double solded_init = solde;
-        double gain;
+        Portefeuille portefeuilleT;
+        portefeuilleT.Alimenter_portefeuille(solde);
+
+        double solded_init = portefeuilleT.Get_solde();
+        cout<<"Trader aleatoire "<<T.get_nom_trader()<<endl;
+        cout << "Solde initial: " << solded_init << endl;
+        double gain = 0;
         int i = 0, nb_achats = 0, nb_ventes = 0, nb_rien = 0;
         auto start = high_resolution_clock::now();
         while (dateact < date_fin)
         {
             if (B.getActionsDisponiblesParDate(dateact).size() > 0)
             {
-                cout << "Aujourd'hui le : " << dateact << endl;
+                if (i==100){
+                    cout<<"Limite journaliere de transaction atteinte pour "<<dateact<<endl;
+                }
+                cout << "Actions Disponibles pour aujourd'hui le : " << dateact << endl;
                 do
                 {
-                    tr = T.choisir_transaction(B, portefeuilleTrader);
-                    portefeuilleTrader.effectuerTransaction(B, tr);
-                    if (tr.GetType() == ACHAT)
+                    tr = T.choisir_transaction(B, portefeuilleT);
+                    string st = tr.GetstringTypeTransaction();
+                    portefeuilleT.effectuerTransaction(B, tr);
+                    if (st == "ACHAT")
                     {
                         nb_achats++;
                         prix_vente[tr.GetTitre().getNomAction()] = B.get_prix_action_auj(tr.GetTitre().getNomAction())->get_prix();
                     }
-                    else if (tr.GetType() == VENTE)
+                    else if (st == "VENTE")
                     {
+                        double recent_price;
+                        if (B.get_prix_action_auj(tr.GetTitre().getNomAction()) == nullptr)
+                        {
+                            cout << "Pas de prix disponible pour la vente de cette action aujourd'hui" << endl;
+                            cout << "Searching for most recent price" << endl;
+                            recent_price = B.get_dernier_prix_action(dateact, tr.GetTitre().getNomAction());
+                        }
+                        else
+                        {
+                            recent_price = B.get_prix_action_auj(tr.GetTitre().getNomAction())->get_prix();
+                        }
+                        prix_vente[tr.GetTitre().getNomAction()] = recent_price;
                         nb_ventes++;
-                        prix_vente[tr.GetTitre().getNomAction()] = B.get_prix_action_auj(tr.GetTitre().getNomAction())->get_prix();
+                    }
+                    else if (st == "RIEN")
+                    {
+                        cout<<"Pas de Transaction pour aujourd'hui"<<endl;
+                        nb_rien++;
+                        break;
                     }
                     else
-                        if (tr.GetType() == RIEN)
-                        {
-                            nb_rien++;
-                        }
-                    else
                     {
-                        cout << "Transaction échouée" << endl;
+                        cout << "Transaction echouee" << endl;
                     }
                     i++;
-                } while (tr.GetType() != RIEN && i < 100);
+                } while (i < 200);
             }
+            else
+            {
+                cout << "Bourse est fermeee pour le : " << dateact << " Incrementation de jour en cours" << endl;
+            }
+            dateact.incrementer();
+            B.set_auj(dateact);
         }
-        dateact.incrementer();
-        B.set_auj(dateact);
+
         auto end = high_resolution_clock::now();
         auto duree = duration_cast<microseconds>(end - start);
+        cout << "Simulation has ended" << endl;
 
-        for (Titre tr : portefeuilleTrader.get_actions_possede())
+        gain += portefeuilleT.Get_solde();
+        cout << "Vente de toutes les actions" << endl;
+        for (Titre tr : portefeuilleT.get_actions_possede())
         {
+
             for (auto pv : prix_vente)
             {
                 if (tr.getNomAction() == pv.first)
@@ -81,37 +105,24 @@ public:
                 }
             }
         }
+        portefeuilleT.Alimenter_portefeuille(gain);
+        cout << "Solde initial : " << solded_init << endl;
+        cout << "Solde final = " << gain << endl;
         auto start1 = high_resolution_clock::now();
         B.getActionsDisponiblesParDate(dateact);
         auto end1 = high_resolution_clock::now();
         auto duree1 = duration_cast<microseconds>(end1 - start1);
         stats["nombre d'achats"] = nb_achats;
         stats["nombres de ventes"] = nb_ventes;
+        stats["nombres de Rien"] = nb_rien;
         stats["nombres total de transaction"] = nb_achats + nb_rien + nb_ventes;
-        stats["nombres d'actions"] = portefeuilleTrader.get_actions_possede().size();
-        stats["solde final"] = portefeuilleTrader.Get_solde();
+        stats["nombres d'actions"] = portefeuilleT.get_actions_possede().size();
+        stats["solde final"] = portefeuilleT.Get_solde();
         stats["profit"] = gain - solded_init;
         stats["profit %"] = (100 * (gain - solded_init) / solded_init);
-        stats["Temps d'execution de programme (µs)"] = duree.count();
-        stats["Temps moyen get actions disponible aujoud'hui (µs)"] = duree1.count();
+        stats["Temps d'execution de programme"] = duree.count();
+        stats["Temps moyen get actions disponible aujoud'hui"] = duree1.count();
 
         return stats;
     }
 };
-
-int main()
-{
-    string filepath = "C://Users//zizou//OneDrive//Documents//GitHub//Bourse-Mini-Projet-C--//prices_simple.csv";
-    BourseVector B(filepath);
-    Trader_aleatoire T1;
-    Portefeuille P1;
-    int solde;
-    Date dateDebut(1, 5, 2014);
-    Date datefin(30, 11, 2014);
-    auto stats = Simulation::lancer_simulation(B, T1, dateDebut, datefin, solde);
-    for (auto it : stats)
-    {
-        cout << it.first << "\t" << it.second << endl;
-    }
-    return 0;
-}
