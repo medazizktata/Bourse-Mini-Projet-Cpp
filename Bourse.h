13c368d1dbfa49be8cd8897e2425b6dfb030105a
medazizktata <aziiizktata7@gmail.com>
2023-05-15 13:41:01 +0100

    Update Bourse.h

--- Bourse.h
+++ Bourse.h
@@ -4,6 +4,7 @@
 #include "Date.h"
 #include "PrixJournalier.h"
 using namespace std;
+template <typename structure = vector<string>>
 class Bourse
 {
 protected:
@@ -21,14 +22,15 @@ public:
     }
     virtual ~Bourse() {}
     virtual double get_prix_action(Date d, string nom) const = 0;
-    virtual double get_dernier_prix_action(string nom) const = 0;
+    virtual double get_dernier_prix_action(Date d, string nom) const = 0;
     virtual PrixJournalier *getprixjournalier(Date d, string nom) const = 0;
     PrixJournalier *get_prix_action_auj(string nom) const
     {
         return getprixjournalier(auj, nom);
     }
-    virtual void acces_archive(const Date d, int n, const string nom) = 0;
 
-    virtual vector<string> getActionsDisponiblesParDate(Date &d) const = 0;
+    virtual structure getActionsDisponiblesParDate(Date &d) const = 0;
+    virtual void acces_archive(const Date d, int n, const string nom) = 0;
 };
-#endif
+
+#endif // BOURSE_H_INCLUDED
