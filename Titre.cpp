33862067906132724ef5debab11e1408b674a12b
medazizktata <aziiizktata7@gmail.com>
2023-05-15 14:10:14 +0100

    Update Titre.cpp

--- Titre.cpp
+++ Titre.cpp
@@ -5,36 +5,43 @@
 #include "Titre.h"
 using namespace std;
 
-
-Titre::Titre(string n="",double q=0){
-    nom_action=n;
-    qte=q;
+Titre::Titre(string n, double q)
+{
+    nom_action = n;
+    qte = q;
 }
 
-string Titre::getNomAction() const{
+string Titre::getNomAction() const
+{
     return nom_action;
 }
-double Titre::getQte() const{
+double Titre::getQte() const
+{
     return qte;
 }
-void Titre::setNomAction(string nom){
-    nom_action=nom;
+void Titre::setNomAction(string nom)
+{
+    nom_action = nom;
 }
-void Titre::setQte(double q){
-    qte=q;
+void Titre::setQte(double q)
+{
+    qte = q;
 }
-void Titre::ajouterQte(double q){
-    qte+=q;
+void Titre::ajouterQte(double q)
+{
+    qte += q;
 }
-void Titre::retirerQte(double q){
-    qte-=q;
+void Titre::retirerQte(double q)
+{
+    qte -= q;
 }
 
-ostream& operator<<(ostream& os,const Titre& t){
-    cout<<t.nom_action<<";"<<t.qte<<endl;
+ostream &operator<<(ostream &os, const Titre &t)
+{
+    cout << t.nom_action << ";" << t.qte << endl;
     return os;
 }
-bool operator==(const Titre& t1,const Titre& t2){
-    return (t2.nom_action==t1.nom_action && t1.qte==t2.qte);
+bool operator==(const Titre &t1, const Titre &t2)
+{
+    return (t2.nom_action == t1.nom_action && t1.qte == t2.qte);
 }
-Titre::~Titre(){}
