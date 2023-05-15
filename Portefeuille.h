5e88ad56daf82a9ce335d632820df406fee2f1ac
medazizktata <aziiizktata7@gmail.com>
2023-05-15 13:49:43 +0100

    Update Portefeuille.h

--- Portefeuille.h
+++ Portefeuille.h
@@ -5,6 +5,7 @@
 #include "Trader.h"
 #include "PrixJournalier.h"
 #include "Titre.h"
+#include "Bourse.h"
 
 using namespace std;
 
@@ -13,16 +14,33 @@ class Portefeuille
 private:
     double solde;
     vector<Titre> actionspossed;
+
 public:
-    Portefeuille();
-    Portefeuille(double Soldedebut, vector<Titre> act={});
-    ~Portefeuille();
+    Portefeuille(double Soldedebut, vector<Titre> act);
     double Get_solde() const;
     vector<Titre> get_actions_possede() const;
+    void Alimenter_portefeuille(double somme);
     void Acheteraction(Titre t, double prix);
     void Vendreaction(Titre t, double prix);
-    void effectuerTransaction(Bourse &B, Transaction tt);
-};
+    template <typename typebourse>
+    void effectuerTransaction(typebourse &B, Transaction tt)
+    {
+        Titre tl = tt.GetTitre();
+        string s = tt.GetstringTypeTransaction();
+        if (s == "ACHAT")
+        {
+            Acheteraction(tl, B.get_prix_action_auj(tl.getNomAction())->get_prix());
+        }
+        else if (s == "VENTE")
+        {
+            Vendreaction(tl, B.get_prix_action_auj(tl.getNomAction())->get_prix());
+        }
 
+        else if (s == "RIEN")
+        {
+            return;
+        }
+    }
+};
 
 #endif // PORTFEUILLE_H_INCLUDED
